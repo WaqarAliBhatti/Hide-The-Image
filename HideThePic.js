1ecf0200f410a8186e458cd1559c9148d2ecad16
Waqar Ali <57633448+WaqarAliBhatti@users.noreply.github.com>
2022-08-06 03:44:59 +0500

    Add files via upload

---
+++ HideThePic.js
@@ -0,0 +1,6 @@
+document.querySelector('#Trophy').addEventListener('click',hide);
+function hide(){
+    document.querySelector('#Sri').style.display='none';
+    document.querySelector('#Ind').style.display='none';
+
+}
